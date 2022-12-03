9b025871ff3cd5d2f3720ac011782872eb8bfde3
Ellie Park <elliepark1012@gmail.com>
2022-12-03 14:30:00 -0500

    background image not loaded either style attribute or img src both not working

--- src/Components/ShopListItem.js
+++ src/Components/ShopListItem.js
@@ -2,10 +2,14 @@ const ShopListItem = ( {id, name, location, price, backgourndSrc, link, contact,
 
     return (
       <li className="card"> 
-        <img style = {{width: "200px", height: "200px"}} src={backgourndSrc} alt={name} /> 
+        <img style = {{width: "200px", 
+        height: "200px", 
+        // this line of the code has issue
+        // backgroundImage: url(backgroundSrc) 
+    }} /> 
         <h4>{name}</h4>
         <h4>{location}</h4>
-        <h4>`Rent hourly: ${price}`</h4>
+        <h4>Rent hourly: {price}</h4>
         <h4>{contact}</h4>
         <h4>{rating}</h4>
         <h4>{link}</h4>
