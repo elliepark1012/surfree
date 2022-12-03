8d977b3f57d52d81528eb32b2dfff13eb7917399
Ellie Park <elliepark1012@gmail.com>
2022-12-03 14:14:46 -0500

    added video inside Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,11 @@
+import video from "../assets/mainvideo.mp4"
 const Header = () => {
     return (
       <div className="Header">
         <h1>Surf-free::</h1>
-        <h3>Travel light Enjoy the waves fully</h3>
+        <h3>Enrich Your Surf Trip</h3>
+        <video src= {video} autoPlay loop comuted />
+
       </div>
     )
   }
