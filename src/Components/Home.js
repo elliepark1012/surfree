38f18277e06f6f4368c603c7d90b63888807d8eb
Ellie Park <elliepark1012@gmail.com>
2022-12-14 18:01:56 -0500

    home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,11 @@
 import video from "../assets/mainvideo.mp4";
 
 const Home = () => {
-   
     return (
         <section className="box">
                 <video src= {video} autoPlay loop muted />
             <div className="home-content">
-                <h2>
-                Travel Light <br></br>
-                Surf Free <br></br>
-                Build Community
-                </h2>
+                <h2>Travel Light <br></br>Surf Free <br></br> Build Community</h2>
             </div>
         </section>
     )
