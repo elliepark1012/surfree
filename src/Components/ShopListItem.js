37a45bac89add96d232bc8539e1935d2b68aea79
Ellie Park <elliepark1012@gmail.com>
2022-12-14 18:09:15 -0500

    shoplistitem.js

--- src/Components/ShopListItem.js
+++ src/Components/ShopListItem.js
@@ -1,24 +1,21 @@
 import { React } from "react";
-// import { Link } from "react-router-dom";
 
 const ShopListItem = ({ shop, logIn }) => {
-
     const { name, location, price, backgroundSrc, rating } = shop
-    const priceVariant = logIn? 
-    `$${price}/h` : `🙌 $${price -3}/h 🙌`
+    const priceVariant = logIn? `$${price}/h` : `🙌 $${price -3}/h 🙌`
      
     return (
       <li className="card"> 
         <img className="card-image" src={backgroundSrc} alt={name} /> 
         <h1 className="card-name">{name}</h1>
         <div className="price-rating-grid">
-         <div className="price-rating-grid-box1">{priceVariant}</div>
+          <div className="price-rating-grid-box1">{priceVariant}</div>
           <div className="price-rating-grid-box2">{rating}</div>
           <div className="price-rating-grid-box3">Board Rental</div>
           <div className="price-rating-grid-box4">Average Rating</div> 
         </div>
         <h2 className="card-location">📍 {location} </h2>
-        </li>
+      </li>
     );
   };
 
