733deef7da128b8146abd24ac83557621a07142f
Ellie Park <elliepark1012@gmail.com>
2022-12-14 18:00:59 -0500

    header.js final

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/surfree-logo.png";
 
@@ -17,12 +16,12 @@ const Header = ({ logIn, onToggleLogIn }) => {
           </Link>
           </div>
           <div className="navbar-div">
-          <ul className="navbar">
-            <li><Link className="nav-bar-link" to="/">Home</Link></li>
-            <li><NavLink className="nav-bar-link" exact to="/shops">All Shops</NavLink></li>
-            <li><NavLink className="nav-bar-link" exact to="/shops/new"> Join Us</NavLink></li>
-            <li><button className="nav-bar-button" onClick={onToggleLogIn}>{buttonTextContent}</button></li>
-          </ul>
+            <ul className="navbar">
+              <li><Link className="nav-bar-link" to="/">Home</Link></li>
+              <li><NavLink className="nav-bar-link" exact to="/shops">All Shops</NavLink></li>
+              <li><NavLink className="nav-bar-link" exact to="/shops/new"> Join Us</NavLink></li>
+              <li><button className="nav-bar-button" onClick={onToggleLogIn}>{buttonTextContent}</button></li>
+            </ul>
           </div>
        </nav>
       </header>  
